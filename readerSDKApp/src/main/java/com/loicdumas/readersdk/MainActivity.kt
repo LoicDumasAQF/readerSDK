f4a8a9bc696bce46deae96edaa1cfdef13bf6800
Loic <loic.dumas@rakuten.com>
2018-09-12 11:53:12 +0200

    All il same folder

--- readerSDKApp/src/main/java/com/loicdumas/readersdk/MainActivity.kt
+++ readerSDKApp/src/main/java/com/loicdumas/readersdk/MainActivity.kt
@@ -2,7 +2,6 @@ package com.loicdumas.readersdk
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import com.loicdumas.calculator.Calculator
 import com.loicdumas.readercalculatorlib.ReaderCalculator
 import kotlinx.android.synthetic.main.activity_main.*
 
